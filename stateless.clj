caade0ba9e9c4be10d468c562d1c66f9e9fe8bc6
Anand Patil <anand.prabhakar.patil@gmail.com>
2009-04-26 21:57:58 +0200

    Radically reduced code volume

--- stateless.clj
+++ stateless.clj
@@ -1,44 +1,20 @@
-(defn where [bool-vec]
-    "Like numpy's where."
-    (for [i (range (count bool-vec)) :when (bool-vec i)] i))
-
-(defn build-args [wf args]
-    "Like take-where. Returns a closure that takes a list of arguments, inserts input values."
-    (fn [f-args] (replace args f-args)))
-
-(defn multi-partial [test fun args]
-    "Extension of partial to multiple positional arguments."
-    (let [where-f (map test args)
-            where-nf (apply + where-f)]
-        (cond 
-            ;If all arguments are functions, return inputs.
-            (= where-nf (count args))
-                fun
-            ; If no arguments are functions, close on the answer.
-            (= where-nf 0)
-                (fn [] (apply fun args))
-            ; Otherwise, partially apply the function.
-            true
-                (comp fun (build-args where-f (filter (comp not test) args))))))
-
 ;TODO: Force unique labels.
 (defn add-fn [label fun flow & [args]]
     "Adds fun to the dataflow flow, with label 'label', which must be unique."
-    (let [test #(if (flow %) 1 0)
-        parents (set (filter test args))
-        pfun (multi-partial test fun args)]
-        (loop [new-flow (assoc flow label {:fn pfun :val nil :parents parents :children #{}})
-            parent (first parents)
-            parents (rest parents)]
-            (if parent
-                (recur
-                    (if (new-flow parent)
-                        (assoc new-flow parent (assoc (new-flow parent) :children (conj (:children (new-flow parent)) label)))
-                    new-flow)
-                    (first parents)
-                    (rest parents))
-                new-flow))))
-
+    (let [
+        ; Parents are nodes in the flow
+        parents (set (filter #(if (flow %) 1 0) args))   
+        ; A partially-applied version of the function that takes nodes only
+        pfun (comp fun #(replace % args))                  
+        ; The flow, with the new function added
+        new-flow (assoc flow label {:fn pfun :val nil :parents parents :children #{}})
+        ; A function adding label to the children list of a parent.
+        reduce-fn (fn [nf p] (assoc nf p (assoc (nf p) :children (conj (:children (nf p)) label))))] 
+        ; Notify parents of new child
+        (if parents
+            (reduce reduce-fn new-flow parents)
+            new-flow)))
+        
 (def fn1 #())       
 (defn fn2 [a b c d e] [a b c d e])     
 (def flow (add-fn :fn1 fn1 {} []))
