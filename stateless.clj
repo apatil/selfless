91da7538a95b7931942c7c480dd568bfc8c08632
Anand Patil <anand.prabhakar.patil@gmail.com>
2009-04-26 22:36:22 +0200

    Blocking nodes

--- stateless.clj
+++ stateless.clj
@@ -7,9 +7,9 @@
             (for [field fields] (let [n (accessor-names field) s (symbol n)]
                 `(def ~s (accessor ~sym ~field))))))))
                 
-(structmap-and-accessors node :fn :val :parents :children)
+(structmap-and-accessors node :fn :val :parents :children :block?)
 
-(defn add-fn [label fun flow & [args]]
+(defn add-node [label fun flow block? & [args]]
     "Adds fun to the dataflow flow, with label 'label', which must be unique."
     (if (flow label) 
         (throw (Exception. (.concat "Node label already taken: " (name label))))
@@ -19,18 +19,21 @@
             ; A partially-applied version of the function that takes nodes only
             pfun #(apply fun (replace % args))                  
             ; The flow, with the new function added
-            new-flow (assoc flow label (struct node pfun nil parents #{}))
+            new-flow (assoc flow label (struct node pfun nil parents #{} block?))
             ; A function adding label to the children list of a parent.
             add-child (fn [nf p] (assoc nf p (assoc (nf p) :children (conj (:children (nf p)) label))))] 
             ; Notify parents of new child
             (if parents
                 (reduce add-child new-flow parents)
                 new-flow))))
+                
+(defn change-flow [flow val-map])
         
 (def fn1 #())       
 (defn fn2 [a b c d e] [a b c d e])     
 (defn fn3 [fn2] (apply + fn2))
-(def flow (add-fn :fn1 fn1 {} []))
-(def flow2 (add-fn :fn2 fn2 flow [:fn1 3 :fn1 2 5]))
-(def flow3 (add-fn :fn3 fn3 flow2 [:fn2]))
-(def flow3 (add-fn :fn3 fn3 flow3 [:fn2]))
+(def flow (add-node :fn1 fn1 {} false []))
+(def flow2 (add-node :fn2 fn2 flow false [:fn1 3 :fn1 2 5]))
+(def flow3 (add-node :fn3 fn3 flow2 false [:fn2]))
+; Error here
+(def flow3 (add-node :fn3 fn3 flow3 [:fn2]))
