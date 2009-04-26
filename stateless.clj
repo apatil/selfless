ef95bc6e2fc462ba86ba7c731c98e6c3aa00dd21
Anand Patil <anand.prabhakar.patil@gmail.com>
2009-04-26 22:43:32 +0200

    Took val out of the flow... duh

--- stateless.clj
+++ stateless.clj
@@ -7,7 +7,7 @@
             (for [field fields] (let [n (accessor-names field) s (symbol n)]
                 `(def ~s (accessor ~sym ~field))))))))
                 
-(structmap-and-accessors node :fn :val :parents :children :block?)
+(structmap-and-accessors node :fn :parents :children :block?)
 
 (defn add-node [label fun flow block? & [args]]
     "Adds fun to the dataflow flow, with label 'label', which must be unique."
@@ -19,15 +19,23 @@
             ; A partially-applied version of the function that takes nodes only
             pfun #(apply fun (replace % args))                  
             ; The flow, with the new function added
-            new-flow (assoc flow label (struct node pfun nil parents #{} block?))
+            new-flow (assoc flow label (struct node pfun parents #{} block?))
             ; A function adding label to the children list of a parent.
             add-child (fn [nf p] (assoc nf p (assoc (nf p) :children (conj (:children (nf p)) label))))] 
             ; Notify parents of new child
             (if parents
                 (reduce add-child new-flow parents)
                 new-flow))))
-                
-(defn change-flow [flow val-map])
+
+(defn notify [flow flow-map label]
+    "Notifies the children of a label that it has changed. 
+    Sets their values to nil, and propagates the 'message' 
+    to their children."
+    (reduce #(  ) flow (get-children (flow label))))
+
+(defn change-flow [flow flow-map new-val-map]
+    "Sets the flow's value at certain labels to new values."
+    (reduce #(  ) flow val-map))
         
 (def fn1 #())       
 (defn fn2 [a b c d e] [a b c d e])     
